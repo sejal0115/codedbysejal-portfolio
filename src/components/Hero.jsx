88daac6ff2fd0b1ec64c5f9df59078759355b302
Sejal Mahadik <sejalmahadik1519@gmail.com>
2025-08-21 16:38:28 +0530

    CV Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -69,7 +69,7 @@ const Hero = () => {
                                 <motion.a
                                     whileHover={{ scale: 1.05 }}
                                     whileTap={{ scale: 0.95 }}
-                                    href="/Sejal Mahadik CV_2025.pdf"
+                                    href="/SejalMahadik_CV.pdf"
                                     download="Sejal_Mahadik_Resume.pdf" // File name for download
                                     className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 shadow-xl cursor-pointer"
                                 >
